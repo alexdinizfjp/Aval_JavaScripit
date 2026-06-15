c7eea61c86ed3f0a7278c00175fd1d2f740eb82c
Alex Diniz <alexdinizfjp@gmail.com>
2026-06-14 21:57:14 -0300

    Questao1_d.js

---
+++ Questao1_d.js
@@ -0,0 +1,32 @@
+const alunos = [
+  { nome: "Ana", idade: 19, notas: [8, 7, 10], ativo: true },
+  { nome: "Bruno", idade: 17, notas: [5, 6, 4], ativo: true },
+  { nome: "Carlos", idade: 22, notas: [9, 8, 9], ativo: false },
+  { nome: "Daniela", idade: 25, notas: [10, 9, 10], ativo: true }
+];
+
+const relatorioAlunos = alunos.map(aluno => {
+  
+  const somaNotas = aluno.notas.reduce((acc, nota) => acc + nota, 0);
+  const mediaCalculada = somaNotas / aluno.notas.length;
+  
+  let statusSituacao = "REPROVADO";
+  if (mediaCalculada >= 7) {
+    statusSituacao = "APROVADO";
+  }
+
+  return {
+    nome: aluno.nome,
+    media: Number(mediaCalculada.toFixed(2)),
+    situacao: statusSituacao
+  };
+});
+
+console.log("=== EXIBIÇÃO DO RELATÓRIO FINAL (Utilizando for...of) ===");
+
+for (const aluno of relatorioAlunos) {
+
+  console.log(
+    `Discente: ${aluno.nome.padEnd(10, ' ')} | Média: ${aluno.media.toFixed(2)} | Situação: ${aluno.situacao}`
+  );
+}
